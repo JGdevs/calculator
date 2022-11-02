afd8c26d4c0a1ee90018bc5e90a8527f8109c480
Gabriel Saez <gabrielsaez2016@gmail.com>
2022-11-02 18:29:08 -0400

    first commit calculator

---
+++ src/components/DigitButton.jsx
@@ -0,0 +1,13 @@
+import {ACTIONS} from './CalculatorApp'
+
+function DigitButton({dispatch,digit}) {
+
+	return (
+
+		<button onClick={() => dispatch({type:ACTIONS.ADD_DIGIT,payload:{digit}})}>{digit}</button>
+
+	);
+
+}
+
+export default DigitButton;
